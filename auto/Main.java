952702be29cb6807e7352937f0e694606961ab35
ALEJANDRO-SERRANO-S <alexandroserrano286@gmail.com>
2026-02-26 01:13:52 -0600

    Tarea3

---
+++ auto/Main.java
@@ -0,0 +1,43 @@
+
+
+/**
+ * Class Main
+ */
+public class Main {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public Main () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @param        args
+   */
+  public static void main(String[] args)
+  {
+	  Auto auto = new Auto("Ferrari");
+	  Persona persona = new Persona("Alejandro", auto);
+
+	  persona.mostrarAuto();
+  }
+
+
+}
